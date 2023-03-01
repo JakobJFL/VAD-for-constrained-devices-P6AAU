a3b15de99c1767d805f0154f0417f30c5ca9bae4
Jakob Frederik Lykke <47578462+JakobJFL@users.noreply.github.com>
2023-03-01 16:12:56 +0100

    mic stream and pre-trained model
    Added functionality for recording microphone in a stream. Got pretrained model for VAD to work with mic data.

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('Hej Verden')
+from record_audio import start_recording
+from voice_detection import voice_activation_detection
+
+FRAME_RATE = 16000
+RECORD_SECONDS = 2
+
+def main():
+    start_recording(voice_activation_detection, RECORD_SECONDS, FRAME_RATE)
+
+main()
